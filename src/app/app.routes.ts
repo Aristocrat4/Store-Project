33d41326bb2f8049e14963e3d58fe48b370640db
GalaktionDanelia <galadanelia1@gmail.com>
2024-02-08 02:22:43 +0400

    add login page and fix some issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'product', component: ProductDetailsComponent },
+  { path: 'auth', component: AuthComponent },
 ];
