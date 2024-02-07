bf809b6c31b870d00f394b2dbc196337c7ac3c17
Lukabotch <99355744+Lukabotch@users.noreply.github.com>
2024-02-07 16:20:03 +0400

    added product page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../ui/carousel/carousel.component';
+import { CardComponent } from '../../ui/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent,CardComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+products = Array(16)
 }
